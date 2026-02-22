8e32f182ec5eae60e40293cd6ada8944282704c1
M Umar <umarabdulhaque@gmail.com>
2026-02-22 07:55:03 +0500

    feat:remove the export static now checking in vercel

--- next.config.ts
+++ next.config.ts
@@ -4,11 +4,6 @@ import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin("./i18n/request.ts");
 
 const nextConfig: NextConfig = {
-  output: "export",
-  trailingSlash: true,
-  images: {
-    unoptimized: true,
-  },
   typescript: {
     ignoreBuildErrors: true,
   },
