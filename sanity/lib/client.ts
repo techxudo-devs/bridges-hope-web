b16ff3709ca291f48a5fc7ec32c1839366fee622
M Umar <umarabdulhaque@gmail.com>
2026-02-22 08:04:25 +0500

    feat:remove the export static now checking in vercel

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -2,10 +2,12 @@ import { createClient } from "next-sanity";
 
 const projectId =
   process.env.NEXT_PUBLIC_SANITY_PROJECT_ID ||
+  process.env.SANITY_STUDIO_PROJECT_ID ||
   process.env.SANITY_PROJECT_ID ||
   "";
 const dataset =
   process.env.NEXT_PUBLIC_SANITY_DATASET ||
+  process.env.SANITY_STUDIO_DATASET ||
   process.env.SANITY_DATASET ||
   "production";
 
