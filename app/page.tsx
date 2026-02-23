bcaa3f201cf68a06ce0b42cb961f55ff04652d33
M Umar <umarabdulhaque@gmail.com>
2026-02-23 14:09:15 +0500

    fix:redirect issue coming

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,7 @@
-"use client";
-
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
+import { redirect } from "next/navigation";
 
 import { defaultLocale } from "@/i18n";
 
 export default function RootPage() {
-  const router = useRouter();
-
-  useEffect(() => {
-    router.replace(`/${defaultLocale}`);
-  }, [router]);
-
-  return (
-    <main style={{ padding: "2rem", textAlign: "center" }}>
-      <p>Redirecting to your language...</p>
-      <a href={`/${defaultLocale}`}>Go to the site</a>
-    </main>
-  );
+  redirect(`/${defaultLocale}`);
 }
